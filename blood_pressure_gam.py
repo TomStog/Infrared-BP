8eae9268082e1845681d97c8649a993180311994
Thomas Stogiannopoulos <82231949+TomStog@users.noreply.github.com>
2023-10-17 13:48:38 +0300

    Update blood_pressure_gam.py

--- blood_pressure_gam.py
+++ blood_pressure_gam.py
@@ -2,11 +2,9 @@
 
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import KFold
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import r2_score
 from pygam import LinearGAM
 from statistics import mean
-import matplotlib.pyplot as plt
 import statistics
 
 def calculate_percentage_less_than(arr, value):
